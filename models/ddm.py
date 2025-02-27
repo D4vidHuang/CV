c5ed521855ec431024afb51203125d3c8fce4166
yongchenghuang <Y.Huang-51@student.tudelft.nl>
2025-02-27 18:06:06 +0100

    Update Loss
    update loss

--- models/ddm.py
+++ models/ddm.py
@@ -8,7 +8,7 @@ import torch.nn as nn
 import torch.utils.data as data
 import torch.backends.cudnn as cudnn
 from matplotlib import pyplot as plt
-
+from pytorch_msssim import ssim
 import utils
 from models.unet import DiffusionUNet, DiffusionUNet_frequency
 from models.transformer2d import My_DiT_test
@@ -20,6 +20,104 @@ from models.Uformer import create_uformer_nets, create_uformer_nets_frequency
 from models.restormer import create_restormer_nets
 from models.atgan import create_atgan_nets
 
+# New SSIMloss and VGG Perceptual Loss
+class SSIMLoss(nn.Module):
+    def __init__(self):
+        super(SSIMLoss, self).__init__()
+        self.C1 = 0.01**2
+        self.C2 = 0.03**2
+        self.kernel_size = 11
+        self.sigma = 1.5
+        self.kernel = self._create_gaussian_kernel(self.kernel_size, self.sigma)
+        self.kernel = self.kernel.view(1, 1, self.kernel_size, self.kernel_size)
+    
+    def forward(self, img1, img2):
+        (_, channel, _, _) = img1.size()
+        ssim_val = 0
+        for i in range(channel):
+            ssim_val += self._ssim(img1[:, i:i+1, :, :], img2[:, i:i+1, :, :])
+        return (1 - ssim_val / channel).mean()  # (1 - SSIM) as loss
+    
+    def _ssim(self, img1, img2):
+        mu1 = F.conv2d(img1, self.kernel, padding=self.kernel_size//2, groups=1)
+        mu2 = F.conv2d(img2, self.kernel, padding=self.kernel_size//2, groups=1)
+        
+        mu1_sq = mu1.pow(2)
+        mu2_sq = mu2.pow(2)
+        mu1_mu2 = mu1 * mu2
+        
+        sigma1_sq = F.conv2d(img1 * img1, self.kernel, padding=self.kernel_size//2, groups=1) - mu1_sq
+        sigma2_sq = F.conv2d(img2 * img2, self.kernel, padding=self.kernel_size//2, groups=1) - mu2_sq
+        sigma12   = F.conv2d(img1 * img2, self.kernel, padding=self.kernel_size//2, groups=1) - mu1_mu2
+        
+        ssim_map = ((2 * mu1_mu2 + self.C1) * (2 * sigma12 + self.C2)) / ((mu1_sq + mu2_sq + self.C1) * (sigma1_sq + sigma2_sq + self.C2))
+        return ssim_map.mean(dim=(1,2,3))
+
+    def _create_gaussian_kernel(self, kernel_size, sigma):
+        coords = torch.arange(kernel_size).float() - kernel_size//2
+        g = torch.exp(-(coords**2)/(2*sigma**2))
+        g /= g.sum()
+        kernel2d = g.unsqueeze(0) * g.unsqueeze(1)
+        return kernel2d
+    
+class VGGPerceptualLoss(nn.Module):
+    def __init__(self, resize=False):
+        super(VGGPerceptualLoss, self).__init__()
+        vgg = models.vgg19(weights=models.VGG19_Weights.IMAGENET1K_V1).features
+        for param in vgg.parameters():
+            param.requires_grad = False
+
+        self.vgg_blocks = nn.Sequential(
+            vgg[0],  
+            vgg[1], 
+            vgg[2], 
+            vgg[3], 
+            vgg[4], 
+            vgg[5], 
+            vgg[6], 
+            vgg[7], 
+            vgg[8], 
+            vgg[9],
+            vgg[10], 
+            vgg[11],
+            vgg[12],
+            vgg[13], 
+            vgg[14],
+            vgg[15],
+            vgg[16],
+            vgg[17],
+            vgg[18], 
+            vgg[19],
+            vgg[20], 
+            vgg[21],
+        )
+        self.register_buffer('mean', torch.tensor([0.485, 0.456, 0.406]).view(1,3,1,1))
+        self.register_buffer('std',  torch.tensor([0.229, 0.224, 0.225]).view(1,3,1,1))
+        self.resize = resize
+
+    def forward(self, x, y):
+        x = (x - self.mean) / self.std
+        y = (y - self.mean) / self.std
+
+        x_feats = self._extract_features(x)
+        y_feats = self._extract_features(y)
+        loss = 0
+        for xf, yf in zip(x_feats, y_feats):
+            loss += F.l1_loss(xf, yf)
+        return loss
+
+    def _extract_features(self, x):
+        feats = []
+        out = self.vgg_blocks[:4](x)
+        feats.append(out)
+        out = self.vgg_blocks[4:9](out)
+        feats.append(out)
+        out = self.vgg_blocks[9:14](out)
+        feats.append(out)
+        out = self.vgg_blocks[14:22](out) 
+        feats.append(out)
+        return feats
+    
 
 # This script is adapted from the following repositories
 # https://github.com/ermongroup/ddim
@@ -266,7 +364,10 @@ class DenoisingDiffusion(object):
         cudnn.benchmark = True
         train_loader, val_loader = DATASET.get_loaders()
         LossL1 = torch.nn.L1Loss(reduce=True, size_average=True)
-        CharLoss = CharbonnierLoss()
+        #CharLoss = CharbonnierLoss()
+        self.charbonnier = CharbonnierLoss()
+        self.ssim_loss = SSIMLoss()
+        self.vgg_loss = VGGPerceptualLoss(resize=False).to(self.device)
 
         if os.path.isfile(self.args.resume):
             self.load_ddm_ckpt(self.args.resume)
@@ -321,40 +422,73 @@ class DenoisingDiffusion(object):
                     loss = loss1 + loss2
 
                 else:     
+                    # x = x.to(self.device)
+                    # x = data_transform(x)
+
+                    # # print('X', x.shape)
+                    # X_input = x[:,:3,:,:]
+                    # X_GT    = x[:,3:,:,:]
+                    # # img_input = X_input[0].permute(1, 2, 0).cpu().numpy()  # [H, W, C]
+                    # # img_GT = X_GT[0].permute(1, 2, 0).cpu().numpy()  # [H, W, C]
+                    # #
+                    # # # Plot both images side by side
+                    # # fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+                    # #
+                    # # axes[0].imshow(img_input)
+                    # # axes[0].set_title("Input Image")
+                    # # axes[0].axis("off")
+                    # #
+                    # # axes[1].imshow(img_GT)
+                    # # axes[1].set_title("Ground Truth Image")
+                    # # axes[1].axis("off")
+                    # #
+                    # # plt.show()
+                    # # print('X_input',X_input.shape)
+                    # # print('X_GT',X_GT.shape)
+                    # X_output = self.model(X_input)
+                    # # print('X_OUTPUT', X_output.shape)
+                    # # print('-X_output-',X_output.shape,X_input[:,:,midframe,:,:].shape)
+                    # # lossl1 = LossL1(X_output, X_GT)
+                    # lossl1 = CharLoss(X_output, X_GT)
+
+
                     x = x.to(self.device)
+                    # 先把图像映射到[-1,1]
                     x = data_transform(x)
 
-                    # print('X', x.shape)
                     X_input = x[:,:3,:,:]
                     X_GT    = x[:,3:,:,:]
-                    # img_input = X_input[0].permute(1, 2, 0).cpu().numpy()  # [H, W, C]
-                    # img_GT = X_GT[0].permute(1, 2, 0).cpu().numpy()  # [H, W, C]
-                    #
-                    # # Plot both images side by side
-                    # fig, axes = plt.subplots(1, 2, figsize=(10, 5))
-                    #
-                    # axes[0].imshow(img_input)
-                    # axes[0].set_title("Input Image")
-                    # axes[0].axis("off")
-                    #
-                    # axes[1].imshow(img_GT)
-                    # axes[1].set_title("Ground Truth Image")
-                    # axes[1].axis("off")
-                    #
-                    # plt.show()
-                    # print('X_input',X_input.shape)
-                    # print('X_GT',X_GT.shape)
+
+                    # 网络输出
                     X_output = self.model(X_input)
-                    # print('X_OUTPUT', X_output.shape)
-                    # print('-X_output-',X_output.shape,X_input[:,:,midframe,:,:].shape)
-                    # lossl1 = LossL1(X_output, X_GT)
-                    lossl1 = CharLoss(X_output, X_GT)
-                    loss = lossl1
-                    total_loss += lossl1
+
+                    # NOTE: 记得 inverse_data_transform 再送入SSIM和VGGLoss, 因为它们一般需要[0,1]区间
+                    # CharbonnierLoss 可用在[-1,1]也行，但通常建议都放到[0,1]比较直观
+                    X_output_01 = inverse_data_transform(X_output)
+                    X_GT_01     = inverse_data_transform(X_GT)
+                    # 1) Charbonnier loss in [0,1] domain
+                    loss_char = self.charbonnier(X_output_01, X_GT_01)
+
+                    # 2) SSIM loss
+                    loss_ssim = self.ssim_loss(X_output_01, X_GT_01)
+
+                    # 3) VGG perceptual loss
+                    loss_vgg = self.vgg_loss(X_output_01, X_GT_01)
+                    w_char, w_ssim, w_vgg = 1.0, 0.5, 0.1
+                    loss = w_char * loss_char + w_ssim * loss_ssim + w_vgg * loss_vgg
+
+                    #loss = lossl1
+                    total_loss += loss
                     count+=1
 
                 if self.step % 10 == 0:
-                    print(f"step: {self.step}, loss: {loss.item()}, data time: {data_time / (i+1)}")
+                    #print(f"step: {self.step}, loss: {loss.item()}, data time: {data_time / (i+1)}")
+                    print(f"step: {self.step}, "
+                            f"loss: {loss.item():.5f}, "
+                            f"L_char={loss_char.item():.5f}, "
+                            f"L_ssim={loss_ssim.item():.5f}, "
+                            f"L_vgg={loss_vgg.item():.5f}, "
+                            f"data time: {data_time / (i+1):.3f}")
 
                 self.optimizer.zero_grad()
                 loss.backward()
