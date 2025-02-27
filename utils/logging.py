fd3ddac9b2eb03f25c0ab84fda5bf9552512ec4e
YongchengHuang <Y.Huang-51@student.tudelft.nl>
2025-02-28 00:09:58 +0100

    Fix newLoss bug
    fix weight_only bug

--- utils/logging.py
+++ utils/logging.py
@@ -20,4 +20,4 @@ def load_checkpoint(path, device):
     if device is None:
         return torch.load(path,  weights_only=False)
     else:
-        return torch.load(path, map_location=device)
+        return torch.load(path, map_location=device, weights_only=False)
